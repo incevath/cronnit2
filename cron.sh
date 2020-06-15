8fb8e7a1c918226978e8b9e9c78c8f29ada0e2da
Kristopher Ives <kristopher.ives@gmail.com>
2020-06-15 12:21:01 +0000

    Added timestamps to log outputs.

--- cron.sh
+++ cron.sh
@@ -2,4 +2,4 @@
 
 here=$(readlink -f $(dirname "$0"))
 cd "$here"
-/usr/bin/php cron.php >> cronnit.log 2>> cronnit.log.err
+/usr/bin/php cron.php 1>> >(ts >> cronnit.log) 2>> >(ts >> cronnit.log.err)
