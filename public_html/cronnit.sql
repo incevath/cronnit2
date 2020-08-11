ca1cf54e04e49bf618fd60beed7e9592d6f87ab7
MLanghof <max.langhof@gmx.de>
2020-08-11 21:03:25 +0200

    Updated cronnit.sql.
    Since the last update, the deleted column was added. There's now also a
    combined key for account_id + when for performance reasons.

--- public_html/cronnit.sql
+++ public_html/cronnit.sql
@@ -50,11 +50,13 @@ CREATE TABLE `post` (
   `when_original` int(11) unsigned DEFAULT NULL,
   `when_posted` int(11) unsigned DEFAULT NULL,
   `bulk` tinyint(1) unsigned DEFAULT NULL,
+  `deleted` tinyint(1) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_foreignkey_post_account` (`account_id`),
   KEY `when` (`when`),
   KEY `when_original` (`when_original`),
   KEY `when_posted` (`when_posted`),
+  KEY `account_id_when` (`account_id`,`when`),
   CONSTRAINT `c_fk_post_account_id` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
