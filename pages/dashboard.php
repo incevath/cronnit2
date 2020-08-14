694ba6391bdcae03154214f1b68e72da0d2a9812
Matthew Strasiotto <39424834+mstr3336@users.noreply.github.com>
2020-08-14 23:00:50 +1000

    Add Thumbnails to post list (#29)
    * Add thumbnail grabber function to page rendering
    
    * Ge the thumb for each post to list
    
    * Attempt to render the thumb for each post to list
    
    * Remove unused variables in thumbnail grabber
    
    * Make embeds responsive (i hope)
    
    * Fix missing colon, and inappropiate "private" scoping of function in
    script
    
    * Attempt to render timestamp correctly
    
    * Add deleted to SQL schema
    
    * Revert "Add deleted to SQL schema"
    
    * Revert "Revert "Add deleted to SQL schema""
    
    * Refactor html building for reactive elements
    
    * Fix passing of just string part of post to getThumb
    
    * Unescape html for rendering thumbs
    
    * Increase colspan for "no posts" msg
    
    * Fix styling on thumbnails
    
    * Fix styling on imgur image elements
    
    * Change regex delimeter for thumbnails from backticks
    
    * Remove debug log
    
    Co-authored-by: matthew.strasiotto@gamil.com <matthew.strasiotto@gmail.com>
    Co-authored-by: Max Langhof <max.langhof@gmx.de>

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,6 +2,38 @@
 
 use \RedBeanPHP\R as R;
 
+function getThumb($body) : string {
+  $matches = array();
+  $imgur_pattern = '#^http[s]?://i\.imgur\.com/([[:alnum:]]{7})\.(\w+)$#i';
+  
+  $extension_fix_pat = '`(?<=\.)(mp4|gifv)$`'; 
+
+
+  $div_start = "<div style='max-height:256px;'><div style='position:relative; padding-bottom:147.96%;'>";
+  $div_close = "</div></div>"; 
+
+  $out = ''; 
+  if (preg_match($imgur_pattern, $body, $matches)) {
+    $out = $body;
+    
+    // Replace video urls with static .jpg previews
+    $out = preg_replace($extension_fix_pat, 'jpg', $out);
+    $out = "$div_start<img src='$out' style='position:absolute;top:0;left:0;max-height:256px;'>$div_close";
+    return $out;
+  } 
+
+  $redgifs_pat = '`^http[s]?://redgifs\.com/watch/([[:alnum:]-]+)$`i';
+
+  if (preg_match($redgifs_pat, $body, $matches)) {
+    
+    $data_id = $matches[1];
+    $out = "$div_start<iframe src='https://redgifs.com/ifr/$data_id' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0;max-height:256px;' allowfullscreen></iframe>$div_close</p>";
+    return $out;
+  }
+
+  return $out;
+} 
+
 $account = $this->getAccount();
 $this->vars['account'] = $account;
 $posts = $account->withCondition(' ( deleted IS NULL OR deleted = 0 ) ORDER BY `when` DESC ')->ownPostList;
@@ -26,6 +58,11 @@ case 'calendar':
 case 'list':
 default:
   $this->vars['view'] = 'posts-list.html';
+
+  foreach ($posts as $i => $post) {
+	  $posts[$i]['thumb'] = getThumb($post['body']);
+
+  }
   $this->vars['posts'] = $posts;
   break;
 }
