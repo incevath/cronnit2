87012f0cd2684bf7fe6895ad7ec331e8bfee8dc7
Kristopher Ives <kristopher.ives@gmail.com>
2020-10-31 18:21:55 +0000

    Fix duration authorization parameter for good.
    The duration option was added twice (passed through and also added on top).
    It appears that reddit has started rejecting this kind of request, because nothing
    was changed on cronnit's side here.
    
    Manually adding it once at the end does the trick though.

--- pages/authorize.php
+++ pages/authorize.php
@@ -10,10 +10,17 @@ if (!isset($_GET['code']) && !isset($_GET['error'])) {
   $_SESSION['reddit_oauth_state'] = substr(mt_rand().mt_rand().mt_rand(), 0, 10);
 
   $url = $this->getReddit()->getAuthorizationUrl([
+// According to the readme, the duration should be added here. However, this causes duration to be added twice:
+// - Once in https://github.com/rtheunissen/oauth2-reddit/blob/master/src/Provider/Reddit.php#L165
+// - Once in https://github.com/thephpleague/oauth2-client/blob/master/src/Provider/AbstractProvider.php#L386
+//   (passed through all the way)
+//
 //    'duration' => 'permanent',
     'state' => $_SESSION['reddit_oauth_state'],
     'scope' => 'identity submit'
   ]);
+// Instead, add it afterwards so it only appears once.
+  $url .= "&duration=permanent";
 
   header("Location: $url");
   exit(0);
