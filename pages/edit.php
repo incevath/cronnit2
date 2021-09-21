038657ffaf9e2950d5d875e8eed34db6de19f918
Kristopher Ives <kristopher.ives@gmail.com>
2021-09-21 02:03:41 +0000

    Fixed some variables that may not exist from showing warnings.

--- pages/edit.php
+++ pages/edit.php
@@ -23,7 +23,7 @@ if (!isset($_POST['submit'])) {
   $post->whendate = $this->formatUserDate(intval($post->when), $post->whenzone ?: "UTC");
   $post->whentime = $this->formatUserTime(intval($post->when), $post->whenzone ?: "UTC");
   $this->vars['post'] = $post;
-  $this->vars['error'] = $_SESSION['editerror'];
+  $this->vars['error'] = @$_SESSION['editerror'];
   unset($_SESSION['editerror']);
   return;
 }
@@ -37,7 +37,7 @@ $post->title = strval($_POST['title']);
 $post->body = strval($_POST['body']);
 $post->when = $this->convertTime($_POST['whendate'], $_POST['whentime'], $_POST['whenzone']);
 $post->whenzone = strval($_POST['whenzone']);
-$post->sendreplies = intval($_POST['sendreplies']);
-$post->nsfw = intval($_POST['nsfw']);
+$post->sendreplies = intval(@$_POST['sendreplies']);
+$post->nsfw = intval(@$_POST['nsfw']);
 R::store($post);
 $this->redirect("dashboard");
